9bbb5d9c437c407de9d61ed01a14e5f5081a8dd7
Marci02 <nagymarci.0218@gmail.com>
2024-09-20 13:35:00 +0200

    navbar sticky

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function openNav() {
 function closeNav() {
   document.getElementById("menu").style.width = "0";
 }
+
+function stickyNav() {
+    var headerHeight = document.querySelector("#about").offsetHeight / 2;
+    var navbar = document.querySelector("nav");
+    var scrollValue = window.scrollY;
+  
+    if (scrollValue > headerHeight) {
+      navbar.classList.add("header-sticky");
+    } else {
+      navbar.classList.remove("header-sticky");
+    }
+  }
+  
+  window.addEventListener("scroll", stickyNav);
